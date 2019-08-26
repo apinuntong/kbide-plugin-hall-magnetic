27a412067c0b5c3647686c44b6292e0f63c14b5a
apinuntong <apinun.tong@gmail.com>
2019-08-26 16:50:42 +0700

    ADD  pin
    ADD pin

--- blocks/generators_hall-magnetic.js
+++ blocks/generators_hall-magnetic.js
@@ -4,3 +4,13 @@ Blockly.JavaScript['hall_magnetic_block'] = function(block) {
   var code = '...;\n';
   return code;
 };
+Blockly.JavaScript["adc_pin_dummy_input_esp32"] = function (block) {
+  let dropdown_name = block.getFieldValue("PIN");
+  let code = `${dropdown_name}`;
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};
+Blockly.JavaScript["adc_pin_dummy_input_arduino"] = function (block) {
+  let dropdown_name = block.getFieldValue("PINAR");
+  let code = `${dropdown_name}`;
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};
